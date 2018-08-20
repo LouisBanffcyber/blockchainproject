bc73c58f67e01194ddd99b71b72c457b3fb085ec
louis <louis@banffcyber.com>
2018-08-20 22:35:16 +0800

    Added Emergency Circuit

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,3 +3,9 @@ var SimpleStorage = artifacts.require("./SimpleStorage.sol");
 module.exports = function(deployer) {
   deployer.deploy(SimpleStorage);
 };
+
+var Tuition = artifacts.require("./Tuition.sol");
+
+module.exports = function(deployer) {
+  deployer.deploy(Tuition);
+};
--- truffle.js
+++ truffle.js
@@ -1,3 +1,5 @@
+
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
@@ -6,8 +8,11 @@ module.exports = {
     development: {
       host: "127.0.0.1",
       port: 7545,
+      // port: 9545,
       network_id: "*" // Match any network id
     }
   }
   
 };
+
+
